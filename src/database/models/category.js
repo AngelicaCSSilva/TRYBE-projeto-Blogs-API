f5975481e91747ce79d10d573a46c4c326789564
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-20 17:19:53 -0300

    feat: :card_file_box: Add Category model

---
+++ src/database/models/category.js
@@ -0,0 +1,16 @@
+'use strict';
+const categorySchema = (sequelize, DataTypes) => {
+  const category = sequelize.define("Category", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    name: DataTypes.STRING
+  }, {
+    timestamps: false,
+  });
+  return category;
+};
+
+module.exports = categorySchema;
