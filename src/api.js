7587170bab243eb64cfd1fd838bfcc478bea40b6
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-20 17:50:02 -0300

    feat: :construction: Add router to api,js

--- src/api.js
+++ src/api.js
@@ -5,8 +5,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-
-// ...
+app.use(require('./routes/router'));
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
