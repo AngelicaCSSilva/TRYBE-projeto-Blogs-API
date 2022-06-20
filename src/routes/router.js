b9a53463654a335a121e80c0aae99da0738e2c59
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-20 19:39:14 -0300

    feat: :construction: Add /login endpoint

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const authController = require('../controllers/authController');
 
 const routers = express.Router();
+
+routers.use('/login', authController);
 
 module.exports = routers;
