842546bc95ea228d6be1f8e3a6c58b4da3e15b52
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-20 19:38:33 -0300

    feat: :construction: Add error middleware

--- src/api.js
+++ src/api.js
@@ -1,11 +1,13 @@
+require('express-async-errors');
 const express = require('express');
-
-// ...
+const errorMiddleware = require('./middlewares/error');
 
 const app = express();
 
 app.use(express.json());
 app.use(require('./routes/router'));
+
+app.use(errorMiddleware);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
