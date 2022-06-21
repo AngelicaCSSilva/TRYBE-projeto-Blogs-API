37a4015384e1882daba89cdc72d8764754690ad3
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-21 10:20:33 -0300

    feat: :sparkles: Add create user - userService

---
+++ src/services/user.service..js
@@ -0,0 +1,14 @@
+const { User } = require('../database/models');
+const { authenticate } = require('./auth.service');
+
+const createUser = async ({ displayName, email, password, image }) => {
+  const userRegistered = await User.findOne({ where: { email } });
+  if (userRegistered) {
+    const error = { status: 409, message: 'User already registered' };
+    throw error;
+  }
+  await User.create({ displayName, email, password, image });
+  return authenticate(email, password);
+};
+
+module.exports = { createUser };
