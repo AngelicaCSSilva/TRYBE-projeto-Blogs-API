22ef4b8a00f39769cf89183566ee257991dc0243
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-21 17:36:44 -0300

    feat: :construction: Add get categories - categoryController

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -6,6 +6,12 @@ const createCategory = async (req, res) => {
   res.status(201).json(newCategory);
 };
 
+const getCategories = async (_req, res) => {
+  const categories = await categoryService.getCategories();
+  res.status(200).json(categories);
+};
+
 module.exports = {
   createCategory,
+  getCategories,
 };
