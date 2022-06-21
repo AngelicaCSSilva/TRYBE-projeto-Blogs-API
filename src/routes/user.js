9b2cfc537b58b34e234c728e1808fb66c430d497
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-21 16:44:09 -0300

    feat: :construction: Add GET /:id endpoint

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,5 +1,9 @@
 const express = require('express');
-const { creteUser, getUsers } = require('../controllers/userController');
+const {
+  creteUser,
+  getUsers,
+  getUserById,
+} = require('../controllers/userController');
 const {
   nameValidation,
   emailValidation,
@@ -11,5 +15,6 @@ const routers = express.Router();
 
 routers.post('/', nameValidation, emailValidation, passwordValidation, creteUser);
 routers.get('/', tokenValidation, getUsers);
+routers.get('/:id', tokenValidation, getUserById);
 
 module.exports = routers;
