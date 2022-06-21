070a1b7bc5c27a7881f539d4e1695c9b49b73b41
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-21 16:44:31 -0300

    feat: :construction: Add get user by id - userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -10,7 +10,15 @@ const getUsers = async (_req, res) => {
   res.status(200).json(users);
 };
 
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+  const user = await userService.getUserById(id);
+  if (!user) return res.status(404).json({ message: 'User does not exist' });
+  res.status(200).json(user);
+};
+
 module.exports = {
   creteUser,
   getUsers,
+  getUserById,
 };
