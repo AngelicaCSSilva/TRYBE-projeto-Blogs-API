25b9413bd35985aa55df42d42295d51a312d5601
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-21 17:36:20 -0300

    feat: :construction: Create GET /categories endpoint

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createCategory } = require('../controllers/categoryController');
+const { createCategory, getCategories } = require('../controllers/categoryController');
 const { nameValidation } = require('../middlewares/categoryValidation');
 const tokenValidation = require('../middlewares/tokenValidation');
 
 const routers = express.Router();
 
 routers.post('/', tokenValidation, nameValidation, createCategory);
+routers.get('/', tokenValidation, getCategories);
 
 module.exports = routers;
