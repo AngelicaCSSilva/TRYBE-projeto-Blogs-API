7154d16624c46849b4b3edd79dab4d204d045d67
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-22 18:31:03 -0300

    feat: :sparkles: Add create post - postService

---
+++ src/services/post.service.js
@@ -0,0 +1,42 @@
+const Sequelize = require('sequelize');
+const sequelizeConfig = require('../database/config/config');
+
+const sequelize = new Sequelize(sequelizeConfig.development);
+
+const { BlogPost, PostCategory, Category } = require('../database/models');
+  
+const categoriesValidation = async (categoryIds) => {
+  const result = await Promise.all(
+    categoryIds.map((categoryId) => (
+      Category.findByPk(categoryId)
+    )),
+  );
+
+  if (!result.every((category) => category !== null)) {
+    const error = { status: 400, message: '"categoryIds" not found' };
+    throw error;
+  }
+};
+
+const createPost = async ({ title, content, categoryIds }, userId) => {
+  await categoriesValidation(categoryIds);
+  try {
+    return sequelize.transaction(async (transaction) => {
+      const newPost = await BlogPost.create(
+        { title, content, userId, published: new Date(), updated: new Date() },
+         { transaction },
+        );
+      await Promise.all(
+        categoryIds.map((categoryId) => (
+          PostCategory.create({ postId: newPost.dataValues.id, categoryId }, { transaction })
+        )),
+      );
+      return newPost;
+    });
+  } catch (err) {
+    const error = { status: 500, message: 'Internal error.' };
+    throw error;
+  }
+};
+
+module.exports = { createPost };
