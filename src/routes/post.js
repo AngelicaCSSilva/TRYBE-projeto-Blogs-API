942ef7cd464acb634d8742637d65e1ebda57a0c8
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-22 19:41:40 -0300

    feat: :construction: Create GET /post/:id endpoint

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createPost, getPosts } = require('../controllers/postController');
+const { createPost, getPosts, getPostById } = require('../controllers/postController');
 const { fieldsValidation } = require('../middlewares/postValidation');
 const tokenValidation = require('../middlewares/tokenValidation');
 
@@ -7,5 +7,6 @@ const routers = express.Router();
 
 routers.post('/', tokenValidation, fieldsValidation, createPost);
 routers.get('/', tokenValidation, getPosts);
+routers.get('/:id', tokenValidation, getPostById);
 
 module.exports = routers;
