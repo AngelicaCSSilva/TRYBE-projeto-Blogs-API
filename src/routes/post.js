62604aa24a288974ea4205940916a1a4cba93542
Angélica Silva <angelica.silva9323@gmail.com>
2022-06-23 14:47:11 -0300

    feat: :construction: Create DELETE /post/:id endpoint

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const { createPost, getPosts, getPostById, updatePost } = require('../controllers/postController');
+const {
+  createPost,
+  getPosts,
+  getPostById,
+  updatePost,
+  deletePost,
+} = require('../controllers/postController');
 const { fieldsValidation, updateValidation } = require('../middlewares/postValidation');
 const tokenValidation = require('../middlewares/tokenValidation');
 
@@ -9,5 +15,6 @@ routers.post('/', tokenValidation, fieldsValidation, createPost);
 routers.get('/', tokenValidation, getPosts);
 routers.get('/:id', tokenValidation, getPostById);
 routers.put('/:id', tokenValidation, updateValidation, updatePost);
+routers.delete('/:id', tokenValidation, deletePost);
 
 module.exports = routers;
